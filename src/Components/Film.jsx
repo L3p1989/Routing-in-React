a6df4401da1d4253fb8ddb7cdcc93ec478ecd241
Patrick Hogan <phogan110@live.com>
2018-12-18 21:07:28 -0700

    changed to class comp. set up to render current `Film`

--- src/Components/Film.jsx
+++ src/Components/Film.jsx
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Films = () => {
-  return (
-    <>
-      <h1>placeholder</h1>
-    </>
-  );
-};
+class Film extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      films: {}
+    };
+  }
 
-export default Films;
+  componentDidMount() {
+    fetch("https://ghibliapi.herokuapp.com/films/" + this.props.match.params.id)
+      .then(res => res.json())
+      .then(films => this.setState({ films }));
+  }
+
+  render() {
+    const {
+      title,
+      description,
+      director,
+      producer,
+      release_date
+    } = this.state.films;
+    return (
+      <>
+        <h1>{title}</h1>
+        <h6>Released in {release_date}</h6>
+        <p className="blockquote">{description}</p>
+        <h6>Directed by: {director}</h6>
+        <h6>Produced by: {producer}</h6>
+      </>
+    );
+  }
+}
+
+export default Film;
