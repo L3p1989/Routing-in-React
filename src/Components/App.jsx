723a0fd4a539df7f8337a68220ff7b71621ad99f
Patrick Hogan <phogan110@live.com>
2018-12-18 18:33:31 -0700

    Set up `App` with 'logo' `img` and `Router`

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,9 +1,37 @@
 import React, { Component } from "react";
-import "./App.css";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import HomePage from "./HomePage";
+import Films from "./Films";
+import People from "./People";
+import logo from "./../assets/logo.png";
 
 class App extends Component {
   render() {
-    return <h1>placeholder</h1>;
+    return (
+      <>
+        <h1>
+          <img src={logo} alt="" />
+        </h1>
+        <Router>
+          <>
+            <Link to="/" className="m-2 ">
+              Home
+            </Link>
+            <Link to="/Films" className="m-2">
+              Films
+            </Link>
+            <Link to="/People" className="m-2">
+              People
+            </Link>
+            <Switch>
+              <Route exact path="/" component={HomePage} />
+              <Route exact path="/Films" component={Films} />
+              <Route exact path="/People" component={People} />
+            </Switch>
+          </>
+        </Router>
+      </>
+    );
   }
 }
 
